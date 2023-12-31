7396e8b76859b4926b9f9bd44ce9c66d321bb42f
dark <tanzid@inbox.ru>
2024-01-01 03:15:57 +0600

    updated

--- e_library/settings.py
+++ e_library/settings.py
@@ -126,9 +126,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-    BASE_DIR / 'static',
-]
+
 
 # Base url to serve media files
 MEDIA_URL = '/media/'
